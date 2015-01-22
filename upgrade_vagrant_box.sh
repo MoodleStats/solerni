22d2d7963c14d6b36aa9edbb080e13285320293b
Nicolas CHAUVIN <nicolas.chauvin@gmail.com>
2015-01-22 12:24:00 +0100

    Initial commit

---
+++ upgrade_vagrant_box.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# The directory where this script is located
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+function upgrade_vagrant_box () {
+	vagrant halt
+	vagrant destroy
+	vagrant box remove Solerni2DevBox
+	export http_proxy=http://niceway:3128 && export https_proxy=http://niceway:3128 && export ftp_proxy=http://niceway:3128
+	vagrant up
+}
+
+function main () {
+	upgrade_vagrant_box
+	exit 0;
+}
+
+
+main "$@"
