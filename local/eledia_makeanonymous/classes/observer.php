0585de3c5f32d66476b991f443770a2c83befc9a
Simon Vart <svart.ext@orange.com>
2015-02-27 16:30:28 +0100

    Squashed 'vagrant/solerni/local/makeanonymous/' content from commit f009acbf
    git-subtree-dir: vagrant/solerni/local/makeanonymous
    git-subtree-split: f009acbfc1af24d08717921f0511941144a03d91

---
+++ local/eledia_makeanonymous/classes/observer.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Event observer.
+ *
+ * @package local_eledia_makeanonymous
+ * @author Matthias Schwabe <support@eledia.de>
+ * @copyright 2013 & 2014 eLeDia GmbH
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot.'/local/eledia_makeanonymous/lib.php');
+
+/**
+ * Event observer.
+ */
+class local_eledia_makeanonymous_observer {
+
+    public static function anonymize(\core\event\user_deleted $event) {
+
+        start_anonymous($event->relateduserid);
+    }
+}
