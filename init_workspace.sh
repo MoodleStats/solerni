769fddcc5b0fb3baf073c2aca7c2689e7738e7e6
jean francois Bourillon <jeanfrancois.bourillon@orange.com>
2015-03-03 17:35:45 +0100

    ajout du plugin block_flexpagenav

--- init_workspace.sh
+++ init_workspace.sh
@@ -262,13 +262,13 @@ function create_subtrees () {
 	#init_subtree vagrant/solerni/course/format/flexpage flexpage-source https://github.com/moodlerooms/moodle-format_flexpage.git MOODLE_27_STABLE
 
 		#Flexpage Format Dependencies : block_flexpagenav
-		#init_subtree vagrant/solerni/blocks/flexpagenav flexpagenav-source https://github.com/moodlerooms/moodle-block_flexpagenav.git MOODLE_27_STABLE
+		init_subtree vagrant/solerni/blocks/flexpagenav flexpagenav-source https://github.com/moodlerooms/moodle-block_flexpagenav.git MOODLE_27_STABLE
 
 		#Flexpage Format Dependencies : block_flexpagemod
 		#init_subtree vagrant/solerni/blocks/flexpagemod flexpagemod-source https://github.com/moodlerooms/moodle-block_flexpagemod.git MOODLE_27_STABLE
 
 		#Flexpage Format Dependencies : theme_flexpage
-		init_subtree vagrant/solerni/theme/flexpage flexpagetheme-source https://github.com/moodlerooms/moodle-theme_flexpage.git MOODLE_27_STABLE
+		#init_subtree vagrant/solerni/theme/flexpage flexpagetheme-source https://github.com/moodlerooms/moodle-theme_flexpage.git MOODLE_27_STABLE
 
 		#Flexpage Format Dependencies : local_mr
 		init_subtree vagrant/solerni/local/mr mr-source https://github.com/moodlerooms/moodle-local_mr.git MOODLE_27_STABLE
