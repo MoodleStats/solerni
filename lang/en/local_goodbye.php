049b8c0f087b38eb5e19eae183f33177dc0e8abd
root <root@p-mooc-dev.rd.francetelecom.fr>
2015-03-03 16:26:01 +0100

    Squashed 'vagrant/solerni/local/goodbye/' content from commit 3aeb2e5
    git-subtree-dir: vagrant/solerni/local/goodbye
    git-subtree-split: 3aeb2e5954c5af51bb803dd52e873628bd336e56

---
+++ lang/en/local_goodbye.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Local Redirect
+ *
+ * This local plugin enrols and redirects users to a pre-configured course after login
+ *
+ * @package    local
+ * @subpackage local_goodbye
+ * @copyright  2013 Bas Brands, www.basbrands.nl
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Goodbye';
+$string['enabled'] = 'Enabled';
+$string['enabled_desc'] = 'Enable users deleting their accounts';
+$string['myaccount'] = 'My Account';
+$string['manageaccount'] = 'Delete my account';
+$string['loginerror'] = 'Invalid username or password';
+$string['deleteaccount'] = 'Delete your account';
+$string['userpass'] = 'Enter your username and password';
+$string['noself'] = 'We are sorry, only self registration accounts are allowed to delete their account';
+$string['disabled'] = 'Account removal disabled';
+$string['farewell'] = 'Goodby text';
+$string['farewell_desc'] = 'Text shown on delete account form';
+$string['defaultfarewell'] = 'To delete your account please enter your username and password. <br>Thank you for using Moodle!';
