297cc1c00fa52247f5d0a798f5b28cf37febe247
NICOLAS Charlotte <charlotte.nicolas@orange.com>
2015-04-20 16:06:41 +0200

    Added header @copyright 2015 Orange

--- vagrant/solerni/local/goodbye/version.php
+++ vagrant/solerni/local/goodbye/version.php
@@ -20,10 +20,13 @@
  * This module has been created to provide users the option to delete their account
  *
  * @package    local
- * @subpackage goodbye, delete your moodle account
- * @copyright  2013 Bas Brands, www.basbrands.nl
+ * @subpackage local_goodbye, delete your moodle account
+ * @copyright  2015 Orange
+ *     Fork : 2013 Bas Brands, www.basbrands.nl
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2015031012;
 $plugin->requires = 2014051205;
