2fc63a7e305c2275a67d21bbe2e60efaf48309c9
LE PERF Stephane <stephane.leperf@orange.com>
2015-04-30 08:59:21 +0200

    New enrolment methode (URL link to course) for us_84

---
+++ vagrant/solerni/enrol/orangeinvitation/edit_form.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Adds new instance of enrol_orangeinvitation to specified course
+ * or edits current instance.
+ *
+ * @package    enrol
+ * @subpackage orangeinvitation
+ * @copyright  Orange 2015 based on Jerome Mouneyrac invitation plugin{@link http://www.moodleitandme.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir.'/formslib.php');
+require_once('locallib.php');
+
+class enrol_orangeinvitation_edit_form extends moodleform {
+
+    public function definition() {
+        $mform = $this->_form;
+
+        list($instance, $plugin, $context) = $this->_customdata;
+
+        $mform->addElement('header', 'header', get_string('pluginname', 'enrol_orangeinvitation'));
+
+        $mform->addElement('text', 'name', get_string('custominstancename', 'enrol'));
+        $mform->setType('name', PARAM_NOTAGS);
+
+        $options = array(ENROL_INSTANCE_ENABLED  => get_string('yes'),
+                         ENROL_INSTANCE_DISABLED => get_string('no'));
+        $mform->addElement('select', 'status', get_string('status', 'enrol_orangeinvitation'), $options);
+        $mform->setDefault('status', $plugin->get_config('status'));
+
+        $mform->addElement('static', 'customtext1static', get_string('invitationlink', 'enrol_orangeinvitation'));
+        $mform->addHelpButton('customtext1static', 'invitationlink', 'enrol_orangeinvitation');
+
+        $mform->addElement('hidden', 'customtext1');
+        $mform->setType('customtext1', PARAM_URL);
+        $mform->addElement('hidden', 'id');
+        $mform->setType('id', PARAM_INT);
+        $mform->addElement('hidden', 'courseid');
+        $mform->setType('courseid', PARAM_INT);
+
+        $this->add_action_buttons(true, ($instance->id ? null : get_string('addinstance', 'enrol')));
+
+        $this->set_data($instance);
+    }
+
+    public function validation($data, $files) {
+        global $DB, $CFG;
+        $errors = parent::validation($data, $files);
+
+        list($instance, $plugin, $context) = $this->_customdata;
+
+        return $errors;
+    }
+}
---
+++ vagrant/solerni/enrol/orangeinvitation/lang/fr/enrol_orangeinvitation.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'enrol_orangeinvitation'
+ *
+ * @package    enrol
+ * @subpackage orangeinvitation
+ * @copyright  Orange 2015 based on Jerome Mouneyrac invitation plugin{@link http://www.moodleitandme.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['expiredtoken'] = 'Ce lien n’est plus actif - les inscriptions sont terminées.';
+$string['inviteusers'] = 'Invite users';
+$string['noinvitationinstanceset'] = 'No invitation enrolment instance has been found. Please add an invitation enrol instance to your course first.';
+$string['nopermissiontosendinvitation'] = 'No permission to send invitations';
+$string['pluginname'] = 'URL Invitation';
+$string['pluginname_desc'] = 'Le module d’invitation permet de générer un lien à envoyer par e-mail permettant au utilisateur de s’inscrire au cours.';
+$string['invitationlink'] = 'URL d’invitation au cours';
+$string['invitationlink_help'] = 'Lien à placer dans le mail d’invitation pour s’inscrire au cours. Le lien permet d’arriver sur la page de présentation du cours et l’utilisateur peut alors décider de s’inscrire';
+$string['status'] = 'Autoriser l’invitation par e-mail';
+$string['status_desc'] = 'Permet l’utilisation de l’inscription à un cours par e-mail.';
+$string['linknotactivated'] = 'Le lien vers ce MOOC n’est pas actif';
