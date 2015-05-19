7d55616ca8a1ba39b2fb95f39f4470b9e2c6e4ce
NICOLAS Charlotte <charlotte.nicolas@orange.com>
2015-05-19 15:05:42 +0200

    Add moosh script to us_6

--- vagrant/solerni/solerni-config/default/conf.sh
+++ vagrant/solerni/solerni-config/default/conf.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 # Fichier "default/conf.sh"
 
-# Default role for all users
+# Default role for all users (#us_62-69)
 # Attention : find the role_id of 'solerni_utilisateur'
-moosh config-set defaultuserroleid 11
+moosh config-set defaultuserroleid 9
 
-# Creators' role in new courses
+# Creators' role in new courses (#us_62-69)
 # Attention : find the role_id of 'solerni_course_creator'
-moosh config-set creatornewroleid 9
+moosh config-set creatornewroleid 15
 
-# Restorers' role in courses
+# Restorers' role in courses (#us_62-69)
 # Attention : find the role_id of 'solerni_course_creator'
-moosh config-set restorernewroleid 9
+moosh config-set restorernewroleid 15
 
-# Default role assignment (Plugin Enrolments/Self enrolment)
+# Default role assignment - Plugin Enrolments/Self enrolment (#us_62-69)
 # Attention : find the role_id of 'solerni_apprenant'
 moosh config-set roleid 10 enrol_self
+
+# Self registration (#us_6)
+moosh config-set registerauth email
+
+
+
+
