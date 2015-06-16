6683853d36d1968d4fcb838f94afa7941043f247
Eliane Chiffre <eliane.chiffre@orange.com>
2015-06-16 15:47:51 +0200

    Ajout de format_text() pour la gestion de la langue + modif relecture stéphane

---
+++ vagrant/solerni/local/orange_thematics/lang/en/local_orange_thematics.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English language strings
+ *
+ * @package    local
+ * @subpackage orange_thematics
+ * @copyright  2015 Orange
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Orange thematics';
+$string['thematics'] = 'Manage thematics';
+$string['thematicid'] = 'Id.';
+$string['thematicname'] = 'Thématique';
+$string['thematicname_help'] = 'You must specify the label of the thematic for the desired languages. Example :
+		&lt;span class="multilang" lang="fr"&gt;Ma thématique&lt;/span&gt;
+		&lt;span class="multilang" lang="en"&gt;My thematic&lt;/span&gt;';
+$string['definethematicsheader'] = 'Managing thematics';
+$string['actionthematics_list'] = 'List of thematics';
+$string['confirmdeletethematic'] = 'Are you sure you want to delete the thematic <b>{$a}</b> ?';
+$string['thematicdeleted'] = 'The thematic {$a} has been deleted.';
+$string['impossiblethematicdeleted'] = 'Impossible to delete the thematic {$a}. It is associated to a course.';
