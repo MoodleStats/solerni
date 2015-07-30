670cad0ce66522da0b849f1012b2f9a15e928d1e
Chiffre <eliane.chiffre@orange.com>
2015-07-30 14:48:44 +0200

    Delete customlabel in branch develop

--- vagrant/solerni/mod/customlabel/type/soluce/lang/en/customlabeltype_soluce.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$string['pluginname'] = 'Course element : Soluce';
-$string['typename'] = 'Soluce';
-$string['configtypename'] = 'Enable subtype Soluce';
-$string['soluce'] = 'Soluce';
-$string['initiallyvisible'] = 'Initially visible';
--- vagrant/solerni/mod/customlabel/type/tipsandtricks/lang/en/customlabeltype_tipsandtricks.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$string['pluginname'] = 'Course element : Tips and Tricks';
-$string['typename'] = 'Tips and tricks';
-$string['configtypename'] = 'Enable subtype Tips and tricks';
-$string['tipsandtricks'] = 'Tips and Tricks';
-
