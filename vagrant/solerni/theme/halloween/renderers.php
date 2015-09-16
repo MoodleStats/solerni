394ca1b4be6bece14f622df3bb10159768e2cc8f
LE PERF Stephane <stephane.leperf@orange.com>
2015-09-16 15:43:12 +0200

    Change in renderer to display information in badge block on my page

--- vagrant/solerni/theme/halloween/renderers.php
+++ vagrant/solerni/theme/halloween/renderers.php
@@ -25,5 +25,6 @@
 global $CFG;
 
 require_once('renderers/core_course_renderer.php');
+require_once('renderers/core_badges_renderer.php');
 require_once('renderers/core_renderer.php');
 require_once('renderers/mod_quiz_renderer.php');
