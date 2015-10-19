b253931e724454b02b43e4c518cbcaf92619b3c6
LE PERF Stephane <stephane.leperf@orange.com>
2015-10-19 10:55:58 +0200

    Orange Progress Bar

---
+++ vagrant/solerni/blocks/orange_progressbar/lang/en/block_orange_progressbar.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Orange Progress Bar block English language translation
+ *
+ * @package    block_orange_progressbar
+ * @copyright  Orange 2015
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['no_activities_monitored'] = 'No activities are being monitored.';
+$string['completion_not_enabled'] = 'Completion tracking not enabled for this course';
+$string['pluginname'] = 'Simple Progress Bar';
+$string['config_default_title'] = 'Progress Bar';
+$string['overview'] = 'Overview of activities';
+$string['progres_detail'] = 'Progess details';
+$string['course_page_activity'] = 'Pages marked as "completed"';
