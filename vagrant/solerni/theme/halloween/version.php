84a193dbc4b13eb1e003926e1cab1344ea9c56ba
jean francois Bourillon <jeanfrancois.bourillon@orange.com>
2015-10-26 14:59:14 +0100

    adding mobile sprites for social sharing

--- vagrant/solerni/theme/halloween/version.php
+++ vagrant/solerni/theme/halloween/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015090801;
+$plugin->version   = 2015100101;
 $plugin->requires  = 2014051200;
 $plugin->release  = 2014051300;
 $plugin->maturity  = MATURITY_BETA;
