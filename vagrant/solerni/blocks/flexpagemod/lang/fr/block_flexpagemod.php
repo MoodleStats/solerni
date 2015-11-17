611e78e65d349927fac89d0cb480517571ac7792
LE PERF Stephane <stephane.leperf@orange.com>
2015-11-17 14:42:30 +0100

    French translation for flexpage bloc activity

---
+++ vagrant/solerni/blocks/flexpagemod/lang/fr/block_flexpagemod.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Flexpage Activity Block
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://opensource.org/licenses/gpl-3.0.html.
+ *
+ * @copyright Copyright (c) 2009 Moodlerooms Inc. (http://www.moodlerooms.com)
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @package block_flexpagemod
+ * @author Mark Nielsen
+ */
+
+/**
+ * Plugin language
+ *
+ * @author Orange 2015
+ * @package block_flexpagemod
+ */
+
+$string['cmdisplayerror'] = 'Impossible d\'afficher l\'activité associée, merci de vérifier la configuration du bloc. Message d\erreur : "{$a}"';
+$string['displayactivity'] = 'Activité à afficher';
+$string['displayactivity_help'] = 'L\activité choisie sera affichée dans le bloc.';
+$string['pluginname'] = 'Activité Flexpage';
+$string['flexpagemod:addinstance'] = 'Rajouté un nouveau bloc d\'activité Flexpage';
