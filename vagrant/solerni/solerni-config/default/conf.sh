c4c2a7d3e4991bcf8ea0024dcb4205e6ac4751a4
Vart Simon <svart.ext@orange.com>
2016-01-28 18:29:42 +0100

    Add Moosh for authorize the use of webservices and create, only on MNET HOME, a role and a user for those webservices

--- vagrant/solerni/solerni-config/default/conf.sh
+++ vagrant/solerni/solerni-config/default/conf.sh
@@ -8,21 +8,8 @@ fi
 if [ -f ../conf/env_moosh.cfg ]; then
 . ../conf/env_moosh.cfg
 fi
-# ${CUSTOMER_LOG_DB_HOST}
-# ${CUSTOMER_LOG_DB_NAME}
-# ${CUSTOMER_LOG_DB_USERNAME}
-# ${CUSTOMER_LOG_DB_PASSWORD}
-# ${CUSTOMER_STATS_DB_HOST}
-# ${CUSTOMER_STATS_DB_NAME}
-# ${CUSTOMER_STATS_DB_USERNAME}
-# ${CUSTOMER_STATS_DB_PASSWORD}
-# ${CUSTOMER_PIWIK_URL}
-
-# ${CUSTOMER_STATIC_DIRECTORY}
-# ${GEOIP_FILE_PATH}
 
 # add conf for external logs (#us_289)
-# moosh config-set enabled_stores logstore_standard,logstore_database,logstore_legacy tool_log
 moosh config-set enabled_stores logstore_standard,logstore_database tool_log
 moosh config-set dbdriver native/mysqli logstore_database
 moosh config-set dbhost ${CUSTOMER_LOG_DB_HOST} logstore_database
---
+++ vagrant/solerni/solerni-config/instances/solerni-home/conf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Fichier "default/conf.sh"
+
+# load of ansible variables us_290 us_292 us_71 us_247
+if [ -f ../../conf/env_moosh.cfg ]; then
+. ../../conf/env_moosh.cfg
+fi
+if [ -f ../conf/env_moosh.cfg ]; then
+. ../conf/env_moosh.cfg
+fi
+
+# Web Services Activation On Home
+moosh config-set enablewebservices 1
+
+# Activate REST
+moosh config-set webservicesprotocol rest
+
+#Import Role
+moosh role-import apiuser solerni-config/instances/solerni-home/users_roles/solerniapiuser.xml
+
+# Create API User
+moosh user-create --password apiuser01! --email solerniapiuser@orange.fr --firstname 'API' --lastname 'User' --city 'Paris' --country 'FR' 'api_user'
