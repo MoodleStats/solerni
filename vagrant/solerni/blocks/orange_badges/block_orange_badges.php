8342fee70f40212df807264218e4dadda792dbfe
NICOLAS Charlotte <charlotte.nicolas@orange.com>
2016-02-17 09:52:26 +0100

    us_411 : add new block Orange badges for Mooc and My pages (us_411 + us_433)

---
+++ vagrant/solerni/blocks/orange_badges/block_orange_badges.php
@@ -0,0 +1,129 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Orange Mooc Badges block for displaying badges to users
+ *
+ * @package    block_orange_badges
+ * @copyright  Orange 2015
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir . "/badgeslib.php");
+
+/**
+ * Displays mooc badges
+ */
+class block_orange_badges extends block_base {
+
+    public function init() {
+        $this->title = get_string('pluginname', 'block_orange_badges');
+    }
+
+    public function instance_allow_multiple() {
+        return true;
+    }
+
+    public function has_config() {
+        return false;
+    }
+
+    public function instance_allow_config() {
+        return true;
+    }
+
+    public function applicable_formats() {
+        return array(
+                'admin' => false,
+                'site-index' => false,
+                'course-view' => true,
+                'mod' => false,
+                'my' => true
+        );
+    }
+
+    public function specialization() {
+        global $PAGE;
+
+        $url = $PAGE->url;
+        // If we are on the dashboard (My) then we customized the output for Solerni.
+        if(!empty($url)){
+            $ismypage = ($PAGE->url->compare(new moodle_url('/my/index.php'), URL_MATCH_BASE) ||
+                $PAGE->url->compare(new moodle_url('/my/indexsys.php'), URL_MATCH_BASE));
+
+            if ($ismypage) {
+                $this->title = get_string('pluginname', 'block_orange_badges');
+            } else {
+                $this->title = get_string('titlemooc', 'block_orange_badges');
+            }
+        } else {
+            $this->title = get_string('pluginname', 'block_orange_badges');
+        }
+    }
+
+    public function get_content() {
+        global $USER, $PAGE, $CFG;
+
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        if (empty($this->config)) {
+            $this->config = new stdClass();
+        }
+
+        // Number of badges to display.
+        if (!isset($this->config->numberofbadges)) {
+            $this->config->numberofbadges = 10;
+        }
+
+        // Create empty content.
+        $this->content = new stdClass();
+        $this->content->text = '';
+
+        if (empty($CFG->enablebadges)) {
+            $this->content->text .= get_string('badgesdisabled', 'badges');
+            return $this->content;
+        }
+
+        $courseid = $this->page->course->id;
+        if ($courseid == SITEID) {
+            $courseid = null;
+        }
+
+        // If we are on the dashboard (My) then we customized the output for Solerni.
+        $ismypage = ($PAGE->url->compare(new moodle_url('/my/index.php'), URL_MATCH_BASE) ||
+                $PAGE->url->compare(new moodle_url('/my/indexsys.php'), URL_MATCH_BASE));
+
+        if ($ismypage) {
+            $badges = badges_get_user_badges($USER->id, $courseid, 0, $this->config->numberofbadges);
+        } else {
+            $badges = badges_get_badges(BADGE_TYPE_COURSE, $courseid, '', '', 0, $this->config->numberofbadges, $USER->id);
+        }
+
+        // All badges or Mooc badges.
+        if ($badges) {
+            $output = $this->page->get_renderer('block_orange_badges');
+            $this->content->text = $output->print_badges_list($badges, $USER->id, true);
+        } else {
+            $this->content->text .= get_string('nothingtodisplay', 'block_orange_badges');
+        }
+
+        return $this->content;
+    }
+}
---
+++ vagrant/solerni/blocks/orange_badges/renderer.php
@@ -0,0 +1,131 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Orange Badges block renderer
+ *
+ * @package    block_orange_badges
+ * @copyright  Orange 2015
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+class block_orange_badges_renderer extends plugin_renderer_base {
+
+    // Outputs badges list.
+    public function print_badges_list($badges, $userid, $profile = false, $external = false) {
+        global $USER, $CFG, $PAGE;
+
+        // If we are on the dashboard (My) then we customized the output for Solerni.
+        $ismypage = ($PAGE->url->compare(new moodle_url('/my/index.php'), URL_MATCH_BASE) ||
+                $PAGE->url->compare(new moodle_url('/my/indexsys.php'), URL_MATCH_BASE));
+
+        foreach ($badges as $badge) {
+            // We need to have the course name here for Solerni and we don't have it in the input parameters
+            // We read it even if it should not be done in a renderer. This prevent to create a new block.
+            $cname = "";
+            if ($ismypage) {
+                $course = get_course($badge->courseid);
+                $cname = $course->fullname;
+            }
+
+            if (!$external) {
+                $context = ($badge->type == BADGE_TYPE_SITE) ?
+                        context_system::instance() : context_course::instance($badge->courseid);
+                $bname = $badge->name . " " . $cname;
+                    $imageurl = moodle_url::make_pluginfile_url($context->id, 'badges', 'badgeimage', $badge->id, '/', 'f1', false);
+            } else {
+                $bname = s($badge->assertion->badge->name . " " . $cname);
+                $imageurl = $badge->imageUrl;
+            }
+
+            if ($ismypage) {
+                $name = html_writer::tag('span', $bname, array('class' => 'badge-name')) . html_writer::tag('span', $cname, array('class' => 'course-name'));
+            } else {
+                $name = html_writer::tag('span', $bname, array('class' => 'badge-name'));
+            }
+
+            // image different if is earned or not
+            if(isset($badge->dateissued))
+                $image = html_writer::empty_tag('img', array('src' => $imageurl, 'class' => 'badge-image'));
+            else
+                $image = html_writer::empty_tag('img', array('src' => $imageurl, 'class' => 'badge-image-notearn'));
+            if (!empty($badge->dateexpire) && $badge->dateexpire < time()) {
+                $image .= $this->output->pix_icon('i/expired',
+                                get_string('expireddate', 'badges', userdate($badge->dateexpire)),
+                                'moodle',
+                                array('class' => 'expireimage'));
+                $name .= '(' . get_string('expired', 'badges') . ')';
+            }
+
+            $download = $status = $push = '';
+            if (($userid == $USER->id) && !$profile) {
+                $url = new moodle_url('mybadges.php', array('download' => $badge->id,
+                    'hash' => $badge->uniquehash, 'sesskey' => sesskey()));
+                $notexpiredbadge = (empty($badge->dateexpire) || $badge->dateexpire > time());
+                $backpackexists = badges_user_has_backpack($USER->id);
+                if (!empty($CFG->badges_allowexternalbackpack) && $notexpiredbadge && $backpackexists) {
+                        $assertion = new moodle_url('/badges/assertion.php', array('b' => $badge->uniquehash));
+                        $action = new component_action('click', 'addtobackpack', array('assertion' => $assertion->out(false)));
+                        $push = $this->output->action_icon(new moodle_url('#'),
+                                new pix_icon('t/backpack', get_string('addtobackpack', 'badges')), $action);
+                }
+
+                $download = $this->output->action_icon($url, new pix_icon('t/download', get_string('download')));
+                if ($badge->visible) {
+                        $url = new moodle_url('mybadges.php', array('hide' => $badge->issuedid, 'sesskey' => sesskey()));
+                        $status = $this->output->action_icon($url, new pix_icon('t/hide', get_string('makeprivate', 'badges')));
+                } else {
+                        $url = new moodle_url('mybadges.php', array('show' => $badge->issuedid, 'sesskey' => sesskey()));
+                        $status = $this->output->action_icon($url, new pix_icon('t/show', get_string('makepublic', 'badges')));
+                }
+            }
+
+
+            if ($ismypage) {
+                $mybadgesurl = new moodle_url('/badges/mybadges.php');
+            } else {
+                $mybadgesurl = new moodle_url('/badges/view.php', array('type' => '2', 'id' => $badge->courseid));
+            }
+
+            if (!$profile) {
+                //$url = new moodle_url('badge.php', array('hash' => $badge->uniquehash));
+                $url = $mybadgesurl;
+            } else {
+                if (!$external) {
+                    //$url = new moodle_url('/badges/badge.php', array('hash' => $badge->uniquehash));
+                    $url = $mybadgesurl;
+                } else {
+                    $hash = hash('md5', $badge->hostedUrl);
+                    $url = new moodle_url('/badges/external.php', array('hash' => $hash, 'user' => $userid));
+                }
+            }
+            $actions = html_writer::tag('div', $push . $download . $status, array('class' => 'badge-actions'));
+            $items[] = html_writer::link($url, $image . $actions, array('title' => $bname));
+        }
+
+        // On Dashboard display link to "my badges" page.
+        $linkmybadges = "";
+        if ($ismypage) {
+            $linkmybadges = html_writer::link($mybadgesurl, get_string('mybadges', 'theme_halloween'));
+        } else {
+            $linkmybadges = html_writer::link($mybadgesurl, get_string('allmoocbadges', 'block_orange_badges'));
+        }
+
+        return html_writer::alist($items, array('class' => 'badges')) . $linkmybadges;
+    }
+}
