76af6e73342c4128e69e927000f7c06cc7745fbf
NICOLAS Charlotte <charlotte.nicolas@orange.com>
2016-03-11 11:44:15 +0100

    Moosh : delete 'add command' for badge and course_overview blocks in dashboard

--- vagrant/solerni/solerni-config/default/conf.sh
+++ vagrant/solerni/solerni-config/default/conf.sh
@@ -110,11 +110,9 @@ moosh config-set catalogurl /catalog block_orange_course_dashboard
 moosh config-set hideblockheader 1 block_orange_course_dashboard
 
 # Manage blocks for 'my' page (Dashboard)
-moosh block-add system 0 course_overview my-index content 0
 moosh block-add system 0 orange_course_dashboard my-index content 0
 moosh block-add system 0 calendar_upcoming my-index content 0
 moosh block-add system 0 private_files my-index content 0
-moosh block-add system 0 badges my-index content 0
 moosh block-add system 0 calendar_month my-index content 0
 moosh block-add system 0 news_items my-index content 0
 moosh block-add system 0 orange_last_message my-index content 0
